f05e8d8bdbdf342f57b5bad4c65e8055cede5796
Mike Stucka <stucka@github>
2016-06-09 16:18:27 -0400

    Replace ambiguous language; attempt to show error-prone users

--- pruner.py
+++ pruner.py
@@ -14,18 +14,17 @@ if apikeys[0][1] == "xoxp-1234567890-1234567890-1234567890-1234567890":
     print("You need to configure creds.py for this to work.")
     exit()
 
-class QuitProcessingUser( Exception ):
-    pass
 
 if __name__ == '__main__':
     totalfiles = 0
     for apikey in apikeys:
         userfiles = 0
-        name = apikey[0]
+        user = apikey[0]
         _token = apikey[1]
-        print("Beginning to work on files of " + name)
+        print("Beginning to work on files of " + user)
         firstfile = "ireallyhateslackrightnow"
         done = False
+        userfailures = []
         while not done:
             files_list_url = 'https://slack.com/api/files.list'
             date = str(calendar.timegm((datetime.now() + timedelta(daystoretain * -1)).utctimetuple()))
@@ -38,10 +37,11 @@ if __name__ == '__main__':
                 if firstfile == "ireallyhateslackrightnow":
                     firstfile = f["id"]
                 if firstfile == f["id"] and userfiles > 0:   # If this isn't our first pass through
-                    print("We have problems deleting files for " + name + ". User may need admin access.")
+                    print("We have problems deleting files for " + user + ". User may need admin access.")
+                    userfailures.append(user)
                     done = True
                     break
-                print("\tDeleting " + name + "'s file " + f["name"].encode("utf-8") + "...")
+                print("\tDeleting " + user + "'s file " + f["name"].encode("utf-8") + "...")
                 timestamp = str(calendar.timegm(datetime.now().utctimetuple()))
                 delete_url = "https://" + _domain + ".slack.com/api/files.delete?t=" + timestamp
                 requests.post(delete_url, data = {
@@ -52,4 +52,6 @@ if __name__ == '__main__':
                 userfiles += 1
 
         totalfiles += userfiles
-        print("DONE! ... with " + name + ". User files deleted: " + str(userfiles) + ". Total files deleted: " + str(totalfiles) + ".")
+        print("DONE! ... with " + user + ". User files deleted: " + str(userfiles) + ". Total files deleted: " + str(totalfiles) + ".")
+        if len(userfailures) > 0:
+            print("We had problems deleting files for some users: " + userfailures.split(", ") + ".")
