08b97d3d10dc7155ba2fcd7c574e8f0134cd35eb
Mike Stucka <stucka@github>
2016-10-11 16:30:26 -0400

    Authentication problem fixes. Will not try to have normal users delete other folks' files.

--- pruner.py
+++ pruner.py
@@ -14,27 +14,43 @@ if apikeys[0][1] == "xoxp-1234567890-1234567890-1234567890-1234567890":
     print("You need to configure creds.py for this to work.")
     exit()
 
+# For each apikey, we need to get the user_id from auth.text, and then check for owner or administrative privileges from
+# users.info. If we don't have elevated access, we need to filter files.list by files owned just by that user.
+# If we do have elevated access, we don't filter files.list.
 
 if __name__ == '__main__':
     totalfiles = 0
     userfailures = []
     sizedeleted = 0
+    baseurl = "https://" + _domain + ".slack.com/api/"
     for apikey in apikeys:
         userfiles = 0
         user = apikey[0]
         _token = apikey[1]
         print("Beginning to work on files of " + user)
+        apitest_data = {"token": _token}
+        apitest_response = requests.post(baseurl + "auth.test", data=apitest_data)
+        user_id = apitest_response.json()["user_id"]
+        usersinfo_data = {"token": _token, "user": user_id}
+        usersinfo_response = requests.post(baseurl + "users.info", data=usersinfo_data)
+        userinfo = usersinfo_response.json()["user"]
+        if userinfo["is_admin"] or userinfo["is_owner"]:
+            print(user + " has elevated powers. Searching for all files available.")
+            elevated = True
+        else:
+            elevated = False
         processedfiles = []
         done = False
         while not done:
-            files_list_url = 'https://slack.com/api/files.list'
             date = str(calendar.timegm((datetime.now() + timedelta(daystoretain * -1)).utctimetuple()))
-            data = {"token": _token, "ts_to": date}
-            response = requests.post(files_list_url, data=data)
-            if len(response.json()["files"]) == 0:
+            fileslist_data = {"token": _token, "ts_to": date}
+            if not elevated:
+                fileslist_data['user'] = user_id    # If not admin or owner, limit search to that user's files
+            list_response = requests.post(baseurl + "files.list", data=fileslist_data)
+            if len(list_response.json()["files"]) == 0:
                 done = True
                 break
-            for f in response.json()["files"]:
+            for f in list_response.json()["files"]:
                 if f["id"] in processedfiles:   # If this isn't our first pass through
                     print("We have problems deleting files for " + user + ". User may need admin access.")
                     userfailures.append(user)
@@ -44,12 +60,14 @@ if __name__ == '__main__':
                 processedfiles.append(f["id"])
                 print("\tDeleting " + user + "'s file " + f["name"].encode("utf-8") + "...")
                 timestamp = str(calendar.timegm(datetime.now().utctimetuple()))
-                delete_url = "https://" + _domain + ".slack.com/api/files.delete?t=" + timestamp
-                deleteresponse = requests.post(delete_url, data={
+                delete_url = baseurl + "files.delete?t=" + timestamp
+                delete_data = {
                     "token": _token,
                     "file": f["id"],
                     "set_active": "true",
-                    "_attempts": "1"})
+                    "_attempts": "1"
+                    }
+                deleteresponse = requests.post(delete_url, data=delete_data)
                 if deleteresponse.json()["ok"]: # If we get ok: True
                     userfiles += 1
                     sizedeleted += f["size"]
