82f17e186832b2b8b78613f7902ac34e94f39627
critmcdonald <christian@cmcdonald.com>
2018-07-05 10:33:35 -0500

    adding notes

--- creds.py
+++ creds.py
@@ -3,7 +3,7 @@
 # daystoretain is how many days you want to keep files around. 0 purges everything, and that's bad. Perhaps you want 30 days?
 
 # apikeys gets a list of tuples. First item in the tuple is a quoted name string, like "Mike".
-# second item in the tuple is that person's API token. Get the token here: https://api.slack.com/docs/oauth-test-tokens
+# second item in the tuple is that person's API token. Get the token here: https://api.slack.com/custom-integrations/legacy-tokens
 # separate the tuples with commas. Don't have a comma at the end.
 
 # Next, a note on security. It's bad to be storing a bunch of other people's API keys. It's bad. You shouldn't do it. Security implications are obvious.
--- readme.md
+++ readme.md
@@ -1,6 +1,10 @@
 Slack Pruner
 ============================
 
+NOTE: This uses a deprecated "Legacy token" from the Slack API. Not sure how long they will be honored and what might be done to update. - crit
+
+DON'T COMMIT API TOKENS.
+
 This simple little program breaks about a million basic security rules to delete outdated files from your Slack domain.
 
 More warnings, and information on how to set this up, are available in creds.py.
